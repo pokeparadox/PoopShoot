8c8204d7f3d8ae0d0546de0c94bc18ee7dfbc0cc
Kevin Winfield-Pantoja <pokeparadox@gmail.com>
2021-09-15 21:03:45 +0100

    Added Init Stars
    Added a routine to generate the initial set of stars and point stars.

--- World/StarManager.gd
+++ World/StarManager.gd
@@ -6,29 +6,54 @@ var SmallStar = preload("res://Stars/LittleStar.tscn")
 onready var timer = Timer.new()
 
 func _ready() -> void:
+	InitStarField()
 	add_child(timer)
 	timer.autostart = true
 	UpdateNextSpawn()
 	timer.connect("timeout", self, "on_timer_timeout")
+
+func SpawnStar(pos = Vector2.ZERO):
+	var s = Star.instance()
+	if(pos != Vector2.ZERO):
+		s.global_position = pos
+	else:
+		s.global_position = Vector2(Random.NextIntRange(0, Resolution.GetWidth()),-50)
+	var vec = CalculationCache.DegreesToVector2d(0)
+	vec.x = -vec.x
+	s.setHeading(vec)
+	add_child(s)
+
+func SpawnSmallStars(pos = Vector2.ZERO):
+	var rang := 5
+	if (pos != Vector2.ZERO):
+		rang = 1
+	for i in range(0,rang):
+		var ss = SmallStar.instance()
+		if(pos != Vector2.ZERO):
+			ss.global_position = pos
+		else:
+			ss.global_position = Vector2(Random.NextIntRange(0, Resolution.GetWidth()),-50)
+		var vec = CalculationCache.DegreesToVector2d(0)
+		ss.setHeading(vec)
+		add_child(ss)
+
+func InitStarField():
+	var SCALER := 25
+	for y in range(0, Resolution.GetHeight()/SCALER):
+		for x in range(0, Resolution.GetWidth()/SCALER):
+			var val := Random.NextIntRange(0,100)
+			if(val < 25):
+				SpawnSmallStars(SCALER*Vector2(x,y))
+			elif(val < 20 or val > 98):
+				SpawnStar(SCALER*Vector2(x,y))
 
 func UpdateNextSpawn():
 	timer.set_wait_time(1 + Random.NextIntRange(0,5))
 	timer.start()
 
 func on_timer_timeout():
-	var s = Star.instance()
-	s.global_position = Vector2(Random.NextIntRange(0, Resolution.GetWidth()),-50)
-	var vec = CalculationCache.DegreesToVector2d(0)
-	vec.x = -vec.x
-	s.setHeading(vec)
-	add_child(s)
-
-	for i in range(0,5):
-		var ss = SmallStar.instance()
-		ss.global_position = Vector2(Random.NextIntRange(0, Resolution.GetWidth()),-5)
-		ss.setHeading(vec)
-		add_child(ss)
-
+	SpawnStar()
+	SpawnSmallStars()
 	UpdateNextSpawn()
 
 func _on_Rocket_ChangeDirectionEvent(playerHeading) -> void:
